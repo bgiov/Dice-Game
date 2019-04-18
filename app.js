8d4eb6401bdc1314dc729476fdb22a44115de648
Benjamin Giovannetti <49256657+bgiov@users.noreply.github.com>
2019-04-18 10:29:19 +1000

    Update app.js

--- app.js
+++ app.js
@@ -4,15 +4,15 @@ init();
 
 document.querySelector(".btn-roll").addEventListener("click", function() {
   if (gamePlaying) {
-    // 1. Random number
+    
     var dice = Math.floor(Math.random() * 6) + 1;
 
-    //2. Display the result
+   
     var diceDOM = document.querySelector(".dice");
     diceDOM.style.display = "block";
     diceDOM.src = "dice-" + dice + ".png";
 
-    //3. Update the round score IF the rolled number was NOT a 1
+   
     if (dice !== 1) {
       //Add score
       roundScore += dice;
@@ -28,14 +28,11 @@ document.querySelector(".btn-roll").addEventListener("click", function() {
 
 document.querySelector(".btn-hold").addEventListener("click", function() {
   if (gamePlaying) {
-    // Add CURRENT score to GLOBAL score
     scores[activePlayer] += roundScore;
 
-    // Update the UI
     document.querySelector("#score-" + activePlayer).textContent =
       scores[activePlayer];
 
-    // Check if player won the game
     if (scores[activePlayer] >= 100) {
       document.querySelector("#name-" + activePlayer).textContent = "Winner!";
       document.querySelector(".dice").style.display = "none";
@@ -54,19 +51,12 @@ document.querySelector(".btn-hold").addEventListener("click", function() {
 });
 
 function nextPlayer() {
-  //Next player
   activePlayer === 0 ? (activePlayer = 1) : (activePlayer = 0);
   roundScore = 0;
-
   document.getElementById("current-0").textContent = "0";
   document.getElementById("current-1").textContent = "0";
-
   document.querySelector(".player-0-panel").classList.toggle("active");
   document.querySelector(".player-1-panel").classList.toggle("active");
-
-  //document.querySelector('.player-0-panel').classList.remove('active');
-  //document.querySelector('.player-1-panel').classList.add('active');
-
   document.querySelector(".dice").style.display = "none";
 }
 
